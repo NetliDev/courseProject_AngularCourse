fb2d6f847162bdf9adc4b4831621875ec05e3ef7
Fernando Antonio Tello Sánchez <fernando.antonio.te1@SCMS.ms.corp>
2019-08-27 12:04:02 -0500

    Muestra el detalle de si clickeas item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,10 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>(); // se manda el recipe type porque es el objeto que queremos mostrar
+
   recipes:Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test',
      'https://www.inspiredtaste.net/wp-content/uploads/2019/07/Crispy-Falafel-Recipe-1200.jpg'),
-    new Recipe('A test Recipe', 'This is simply a test',
+    new Recipe('A test Recipe2', 'This is simply a test2',
      'https://www.inspiredtaste.net/wp-content/uploads/2019/07/Crispy-Falafel-Recipe-1200.jpg')
 
   ];
@@ -21,4 +23,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  onRecipeSelected(recipe :Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
